38d72905e25178df06017fecd1c87755843e9405
jpvalery <jpvalery@users.noreply.github.com>
2021-03-04 08:57:19 -0500

    fix: SEO

--- next-seo-config.js
+++ next-seo-config.js
@@ -1,26 +1,29 @@
 const title = "Get in touch with Jp";
 const description = "Let's chat. But first, fill this form for me will ya?";
+const siteUrl = "https://contact.jpvalery.me"
 
 const SEO = {
-  title,
-  description,
-  canonical: "https://contact.jpvalery.me",
+  title: title,
+  description: description,
+  canonical: siteUrl,
   openGraph: {
     type: "website",
     locale: "en_CA",
-    url: "https://contact.jpvalery.me",
-    title,
-    description,
+    url: siteUrl,
+    title: title,
+    description: description,
     images: [
       {
-        url: "https://contact.jpvalery.me/static/images/og-banner.jpg",
-        alt: title,
+        url: 'https://contact.jpvalery.me/static/og-banner.jpg',
+        width: 1920,
+        height: 1280,
+        alt: 'Open Graph Image',
       },
     ],
   },
   twitter: {
     handle: "@jpvalery",
-    site: "contact.jpvalery.me",
+    site: siteUrl,
     cardType: "summary_large_image",
   },
 };
